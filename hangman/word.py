337e4c03fd0a20f7155b93e64b873af4316e2b9d
Matheus Paiva <matheus.paiva@hexis-hub.com>
2018-04-18 16:49:37 +0100

    chore(hangman): Update .getMask to simplify implementation

--- hangman/word.py
+++ hangman/word.py
@@ -1,3 +1,4 @@
+
 class Word:
     def __init__(self, name, category, level):
         self.name = name
@@ -5,10 +6,7 @@ class Word:
         self.level = level
 
     def getMask(self, answers):
-        mask = ''
-        for letter in self.name:
-            mask += (letter + ' ') if letter in answers else '_ '
-        return mask
+        return ' '.join(letter if (letter in answers) else '_' for letter in self.name)
 
     def appendMaskToLabel(self, mask, label):
         return label.format(mask=mask)
